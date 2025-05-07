f1aa66495dd2af227aab8e7dfdc6dc116ac45101
Md. Habibullah <mdhabibullah.work@gmail.com>
2025-05-07 20:39:50 +0600

    problem 2 modified as clean code

--- Problems/problem2.ts
+++ Problems/problem2.ts
@@ -1,4 +1,6 @@
-const filterByRating = (items: { title: string; rating: number }[]): { title: string; rating: number }[] => {
+type Product = { title: string; rating: number };
+
+const filterByRating = (items: Product[]): Product[] => {
     const higherRatings = items.filter(item => item.rating >= 4)
     return higherRatings
 }
@@ -6,7 +8,8 @@ const filterByRating = (items: { title: string; rating: number }[]): { title: st
 const books = [
     { title: "Book A", rating: 4.5 },
     { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
+    { title: "Book C", rating: 5.0 },
+    { title: "Book D", rating: 2.5 },
 ];
 
 console.log(filterByRating(books))
